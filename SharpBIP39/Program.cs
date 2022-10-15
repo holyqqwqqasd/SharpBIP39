b09c5e2bd6bc2870776c3d87f77fdf75ed52ca14
kapehh <mail@kapehh.net>
2022-10-16 01:23:39 +0300

    wtf one

--- SharpBIP39/Program.cs
+++ SharpBIP39/Program.cs
@@ -1,6 +1,14 @@
-﻿using Bitcoin.BIP39;
+﻿using NBitcoin;
+using Bitcoin.BIP39;
+using System;
 
-var bip39 = new BIP39();
+var bip39 = new BIP39("elevator dinosaur switch you armor vote black syrup fork onion nurse illegal trim rocket combine", "", BIP39.Language.English);
 
 Console.WriteLine(bip39.MnemonicSentence);
 Console.WriteLine(bip39.SeedBytesHexString);
+
+var masterKey = ExtKey.CreateFromSeed(bip39.SeedBytes).Derive(KeyPath.Parse("m/44'/60'/0'/0/0"));
+var pubKey44 = masterKey.Neuter();
+
+Console.WriteLine(masterKey.PrivateKey.PubKey.ToString());
+Console.WriteLine(masterKey.GetWif(Network.Main));
