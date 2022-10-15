735819e7d35a11811bdfbee46a4e5ff79fd47b3b
kapehh <mail@kapehh.net>
2022-10-16 01:50:21 +0300

    use libs

--- SharpBIP39/Program.cs
+++ SharpBIP39/Program.cs
@@ -1,14 +1,16 @@
-﻿using NBitcoin;
-using Bitcoin.BIP39;
-using System;
+﻿using System;
+using NBitcoin;
+using Nethereum.Signer;
+using Nethereum.Web3.Accounts;
+using Nethereum.Hex.HexConvertors.Extensions;
+using KeyPath = NBitcoin.KeyPath;
 
-var bip39 = new BIP39("elevator dinosaur switch you armor vote black syrup fork onion nurse illegal trim rocket combine", "", BIP39.Language.English);
+var k = ExtKey.CreateFromSeed(Convert.FromHexString("f8b7cb3a352673be81251fcd04e9e451f7fcafe5605ec8104d86848072c95918ec033e781514c7d70383ffe698826fb316527137074a4bf4fa6263cec3b7466a"));
+k = k.Derive(KeyPath.Parse("m/44'/60'/0'/0/1"));
 
-Console.WriteLine(bip39.MnemonicSentence);
-Console.WriteLine(bip39.SeedBytesHexString);
+var privateKey = new EthECKey(k.PrivateKey.ToHex());
+var account = new Account(privateKey);
 
-var masterKey = ExtKey.CreateFromSeed(bip39.SeedBytes).Derive(KeyPath.Parse("m/44'/60'/0'/0/0"));
-var pubKey44 = masterKey.Neuter();
-
-Console.WriteLine(masterKey.PrivateKey.PubKey.ToString());
-Console.WriteLine(masterKey.GetWif(Network.Main));
+Console.WriteLine(privateKey.GetPrivateKeyAsBytes().ToHex(true));
+Console.WriteLine(privateKey.GetPubKey(true).ToHex(true));
+Console.WriteLine(account.Address);
